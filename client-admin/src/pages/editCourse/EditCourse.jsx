078ca5f63beac5f5b7032c72bc4ad1e4f80ee9cb
yashprasad234 <yashprasad272@gmail.com>
2024-07-02 14:57:18 +0530

    updated editCourse page

--- client-admin/src/pages/editCourse/EditCourse.jsx
+++ client-admin/src/pages/editCourse/EditCourse.jsx
@@ -13,45 +13,79 @@ export default function EditCourse() {
   // const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URI;
 
   return (
-    <div className="h-min lg:h-screen">
-      <div className="w-full lg:h-96 bg-slate-700 px-8 py-12 relative flex flex-col lg:block">
-        <div className="flex flex-col gap-4 ml-16 w-3/4 justify-center lg:w-1/2 mr-8 text-white">
-          <h1 className="text-5xl font-bold">{course.title}</h1>
-          <p className="text-lg">{course.description}</p>
-          <div className="flex flex-col lg:flex-row gap-8 lg:items-center">
-            <p className="bg-yellow-600 text-sm py-1 px-2">BESTSELLER</p>
-            <p className="text-sm">⭐⭐⭐⭐⭐ (3,54,612 ratings)</p>
-            <p className="text-sm">4,25,34 students</p>
+    <div className="lg:h-max grid grid-cols-12 gap-4 p-8 ">
+      <div className="col-span-10 lg:col-span-6 shadow-xl bg-white rounded-2xl">
+        <form className="p-8">
+          <h2 className="text-4xl">YOUR ACCOUNT</h2>
+          <div className="flex flex-col my-8">
+            <label htmlFor="title">TITLE</label>
+            <input
+              type="text"
+              name="text"
+              className="bg-inherit border border-slate-700 h-12 p-4 "
+            />
           </div>
-          <h6 className="text-sm">Created by {course.instructor}</h6>
-          <div className="flex">
-            <p>
-              Last updated {`${new Date().getMonth() + 1}`.padStart(2, "0")}/
-              {new Date().getFullYear()}
-            </p>
+          <div className="flex flex-col my-8">
+            <label htmlFor="description">DESCRIPTION</label>
+            <input
+              type="text"
+              name="description"
+              id=""
+              className="bg-inherit border border-slate-700 h-12 p-4 "
+            />
           </div>
-        </div>
-        <div className="lg:absolute lg:w-1/4 bg-white h-96 lg:z-20 p-0.5 rounded-md flex flex-col gap-2 lg:top-32 lg:right-12 shadow-md">
-          <img
-            src={course.imgURL}
-            alt=""
-            className="border border-slate-800 rounded-t-md"
-          />
-          <div className="flex items-center gap-4 px-4">
-            <p className="text-xl text-black">₹ {course.price}</p>
-            <p className="text-sm line-through text-gray-600">
-              ₹ {course.price * 5}
-            </p>
-            <p className="text-xl text-gray-700">80% off</p>
+          <div className="flex flex-col my-8">
+            <label htmlFor="imgUrl">IMGURL</label>
+            <input
+              type="text"
+              name="imgUrl"
+              id=""
+              className="bg-inherit border border-slate-700 h-12 p-4 "
+            />
           </div>
-          <div className="flex px-4 gap-2">
+          <div className="flex flex-col my-8">
+            <label htmlFor="price">PRICE</label>
+            <input
+              type="number"
+              name="price"
+              id=""
+              className="bg-inherit border border-slate-700 h-12 p-4 "
+            />
+          </div>
+          <div className="flex flex-col my-8">
+            <label htmlFor="duration">DURATION</label>
+            <input
+              type="number"
+              name="duration"
+              id=""
+              className="bg-inherit border border-slate-700 h-12 p-4 "
+            />
+          </div>
+          <button
+            type="submit"
+            className="border block mx-auto w-1/2 border-slate-700 bg-inherit px-4 py-2 hover:bg-slate-800 hover:text-yellow-50 mt-12"
+          >
+            UPDATE
+          </button>
+        </form>
+      </div>
+      <div className="col-span-10 mx-auto lg:col-span-6 shadow-xl bg-white rounded-2xl p-4 flex flex-col justify-center h-max gap-4 w-11/12 mx-auto">
+        <img
+          src={course.imgURL}
+          alt=""
+          className="border border-slate-800 rounded-t-md"
+        />
+        <div className="flex flex-col gap-2 ">
+          <h1 className="text-2xl font-bold text-">{course.title}</h1>
+          <h3 className="text-xl text-">{course.description}</h3>
+          <div className="flex gap-2 items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="size-6 text-red-600"
+              className="size-6"
             >
               <path
                 strokeLinecap="round"
@@ -59,14 +93,25 @@ export default function EditCourse() {
                 d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
-            <p className="text-md text-red-600">
-              <span className="font-bold">23 hours</span> left at this price!
-            </p>
+            <p className="text-xl ">{course.duration} Hours</p>
           </div>
-          <div className="px-4">
-            <button className="border w-full border-slate-800 hover:bg-slate-800 hover:text-white rounded-md py-4">
-              BUY NOW
-            </button>
+
+          <div className="flex gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4 lg:size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <p className="text-xl text-">₹ {course.price}</p>
           </div>
         </div>
       </div>
