15fd47483b8aa4111167d98d0583da867061c86c
yashprasad234 <yashprasad272@gmail.com>
2024-07-02 09:22:55 +0530

    added tailwind + signup + signin page

---
+++ client-admin/src/components/navbar/Navbar.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Navbar() {
+  const [menuMode, setMenuMode] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col sticky top-0 bg-white text-slate-800 z-50">
+      <div className="flex items-center justify-between px-6 pt-4">
+        <div className="flex justify-between">
+          <img src="" alt="" />
+          <h4 className="text-3xl">EduHub</h4>
+        </div>
+        <div className="hidden lg:block">
+          <ul className="flex mx-2 gap-8 py-2">
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              Signup
+            </li>
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/signin");
+              }}
+            >
+              Signin
+            </li>
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/courses");
+              }}
+            >
+              Courses
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-between gap-5 lg:hidden">
+          <button
+            className="cursor-pointer focus:outline focus:outline-2 focus:outline-slate-500 "
+            onClick={() => setMenuMode(!menuMode)}
+          >
+            {menuMode ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+      {menuMode ? (
+        <div>
+          <ul className="flex flex-col items-start mx-6 gap-2.5 py-2">
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              Signup
+            </li>
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/signin");
+              }}
+            >
+              Signin
+            </li>
+            <li
+              className="cursor-pointer border-b border-slate-800 border-double"
+              onClick={() => {
+                navigate("/courses");
+              }}
+            >
+              Courses
+            </li>
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+}
---
+++ client-admin/src/pages/signup/Signup.jsx
@@ -0,0 +1,67 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Signup() {
+  const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URI;
+  const navigate = useNavigate();
+
+  return (
+    <div className="grid grid-cols-12 gap-2 p-4">
+      <div className="col-span-10 lg:col-span-6 p-4">
+        <div className="flex flex-col mt-28">
+          <h1 className="text-6xl font-bold font-title leading-none">
+            CREATE an ACCOUNT
+          </h1>
+          <p className="text-2xl">
+            Create an account in order to purchase courses and access exclusive
+            content.
+          </p>
+        </div>
+      </div>
+      <div className="col-span-12 lg:col-span-6 w-11/12 border border-black rounded-xl px-4 lg:px-24 py-16 bg-white">
+        <form>
+          <h2 className="text-4xl">YOUR ACCOUNT</h2>
+          <div className="flex flex-col my-8">
+            <label htmlFor="email">EMAIL</label>
+            <input
+              type="email"
+              name="email"
+              id=""
+              className="bg-inherit border border-slate-700 h-12"
+            />
+          </div>
+          <div className="flex flex-col my-8">
+            <label htmlFor="password">PASSWORD</label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              className="bg-inherit border border-slate-700 h-12"
+            />
+          </div>
+          <div className="flex flex-col my-8">
+            <label htmlFor="confirmPassword">CONFIRM PASSWORD</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id=""
+              className="bg-inherit border border-slate-700 h-12"
+            />
+          </div>
+          <button
+            type="submit"
+            className="border border-slate-700 bg-inherit px-4 py-2"
+          >
+            SIGNUP
+          </button>
+        </form>
+        <hr className="w-full border border-slate-700 mt-8 mb-8" />
+        <div className="flex justify-between items-center">
+          <p>ALREADY HAVE AN ACCOUNT?</p>
+          <button className="border border-slate-700 px-4 py-2">SIGNIN</button>
+        </div>
+      </div>
+    </div>
+  );
+}
