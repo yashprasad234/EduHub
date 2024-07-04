5265bd469454c1cfb12c6ff44e1f9678716ea1d5
yashprasad234 <yashprasad272@gmail.com>
2024-07-04 10:28:07 +0530

    updated about section of home page

--- client-user/src/pages/home/about/aboutCard/AboutCard.jsx
+++ client-user/src/pages/home/about/aboutCard/AboutCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function AboutCard({ icon, title, description }) {
   return (
-    <div className="flex flex-col gap-4 border-2 border-slate-800 rounded-lg p-12">
+    <div className="flex flex-col gap-4 border-2 border-slate-800 rounded-lg p-8 h-full">
       {icon}
       <h4 className="text-2xl">{title}</h4>
       <p className="text-md">{description}</p>
